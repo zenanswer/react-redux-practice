ba1d7309050624f0ccd0db5bfb10d9692492af1a
xuechenwang <xuechenwang@cienet.com.cn>
2019-05-31 18:03:05 +0800

    Using redux-thunk for loading Recipes data

--- src/middleware/api.js
+++ src/middleware/api.js
@@ -1,6 +1,7 @@
-import { handleActions } from 'redux-actions';
+// import { handleActions } from 'redux-actions';
 
-import { setRecipes, fetchRecipes } from '../actions/recipes';
+// import { setRecipes, fetchRecipes } from '../actions/recipes';
+import { setRecipes } from '../actions/recipes';
 import { setIngredients } from '../actions/ingredients';
 
 const URL = 'https://s3.amazonaws.com/500tech-shared/db.json';
@@ -12,28 +13,40 @@ function fetchData(url, callback) {
     .catch(err => console.log(`Error fetching recipes: ${err}`));
 }
 
-const handleFetchRecipes = dispatch => (
-  handleActions(
-    new Map([
-      [
-        fetchRecipes,
-        () => {
-          fetchData(URL, (data) => {
-            dispatch(setRecipes(data.recipes));
-            dispatch(setIngredients(data.ingredients));
-          });
-          return true;
-        },
-      ],
-    ]),
-    false,
-  )
-);
+// const handleFetchRecipes = dispatch => (
+//   handleActions(
+//     new Map([
+//       [
+//         fetchRecipes,
+//         () => {
+//           fetchData(URL, (data) => {
+//             dispatch(setRecipes(data.recipes));
+//             dispatch(setIngredients(data.ingredients));
+//           });
+//           return true;
+//         },
+//       ],
+//     ]),
+//     false,
+//   )
+// );
 
-const apiMiddleware = ({ dispatch }) => next => (action) => {
-  handleFetchRecipes(dispatch)(null, action);
+// const apiMiddleware = ({ dispatch }) => next => (action) => {
+//   handleFetchRecipes(dispatch)(null, action);
+//   next(action);
+// };
+
+// export default apiMiddleware;
+
+export function initRecipes() {
+  return (dispatch) => {
+    fetchData(URL, (data) => {
+      dispatch(setRecipes(data.recipes));
+      dispatch(setIngredients(data.ingredients));
+    });
+  };
+}
+
+export const apiMiddleware = () => next => (action) => {
   next(action);
 };
-
-
-export default apiMiddleware;
--- src/reduxdemo.jsx
+++ src/reduxdemo.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Provider } from 'react-redux';
 
 import store from './store';
-import { fetchRecipes } from './actions/recipes';
+// import { fetchRecipes } from './actions/recipes';
+import { initRecipes } from './middleware/api';
 import Recipes from './recipe/recipes';
 
-store.dispatch(fetchRecipes());
+// store.dispatch(fetchRecipes());
+store.dispatch(initRecipes());
 
 function ReduxDemo() {
   return (
