917e5df85eaf79e12b77cb300e52a65bab8a3a6f
xuechenwang <xuechenwang@cienet.com.cn>
2019-05-31 16:15:16 +0800

    Using createAction from redux-actions for FSA action creating

--- src/actions/recipes.js
+++ src/actions/recipes.js
@@ -1,13 +1,31 @@
+import { createAction } from 'redux-actions';
+
 import { ADD_RECIPE, FETCH_RECIPES, SET_RECIPES } from '../constants/actionTypes';
 
-export const addRecipe = name => ({
-  type: ADD_RECIPE, name,
-});
+// Using createAction for create the FSA action.
 
-export const fetchRecipes = () => ({
-  type: FETCH_RECIPES,
-});
+// export const addRecipe = name => ({
+//   type: ADD_RECIPE, name,
+// });
+export const addRecipe = createAction(ADD_RECIPE, name => name);
 
-export const setRecipes = recipes => ({
-  type: SET_RECIPES, recipes,
-});
+// export const fetchRecipes = () => ({
+//   type: FETCH_RECIPES,
+// });
+export const fetchRecipes = createAction(FETCH_RECIPES);
+
+// export const setRecipes = recipes => ({
+//   type: SET_RECIPES, recipes,
+// });
+export const setRecipes = createAction(SET_RECIPES, recipes => recipes);
+
+// We can use createActions for creating multi FSA Action Creating Func,
+// but we cannot get the action.type from those func in the reducer func.
+
+// const { addRecipe, fetchRecipes, setRecipes } = createActions({
+//   ADD_RECIPE: name => name,
+//   FETCH_RECIPES: undefined,
+//   SET_RECIPES: recipes => recipes,
+// });
+
+// export { addRecipe, fetchRecipes, setRecipes };
--- src/reducers/recipes.js
+++ src/reducers/recipes.js
@@ -1,12 +1,13 @@
 import { ADD_RECIPE, SET_RECIPES } from '../constants/actionTypes';
 
 const recipesReducer = (recipes = [], action) => {
+  console.log(action);
   switch (action.type) {
     case ADD_RECIPE:
-      return recipes.concat({ name: action.name });
+      return recipes.concat({ name: action.payload });
 
     case SET_RECIPES:
-      return action.recipes;
+      return action.payload;
 
     default:
       return recipes;
