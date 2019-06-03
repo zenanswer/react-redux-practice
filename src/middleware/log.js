2ede2d4f0b5a97fe86851c98f7510aac16d2e560
xuechenwang <xuechenwang@cienet.com.cn>
2019-06-03 11:28:15 +0800

    Add trace for func action (redux-thunk)

--- src/middleware/log.js
+++ src/middleware/log.js
@@ -1,5 +1,10 @@
-const logMiddleware = ({ getState, dispatch }) => next => (action) => {
-  console.log(`Action: ${action.type}`);
+// const logMiddleware = ({ getState, dispatch }) => next => (action) => {
+const logMiddleware = () => next => (action) => {
+  if (action instanceof Function) {
+    console.log(`Action(Function): ${action}`);
+  } else {
+    console.log(`Action: ${action.type}`);
+  }
 
   next(action);
 };
