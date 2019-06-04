0add73c61f2eaf8349d101bb75a5aa5ef5ab423f
xuechenwang <xuechenwang@cienet.com.cn>
2019-06-04 15:25:44 +0800

    Add redux-logger for logging status changing

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import createSagaMiddleware from 'redux-saga';
+import logger from 'redux-logger';
 
 import rootReducer from './reducers/root';
 import logMiddleware from './middleware/log';
@@ -20,6 +21,7 @@ const store = createStore(
     measureMiddleware,
     searchMiddleware,
     sagaMiddleware,
+    logger,
   ),
 );
 
